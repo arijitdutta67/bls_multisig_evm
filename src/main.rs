13fe6fb330a0cf5bbb6e59d4e46fac423af2516f
Arijit Dutta <arijit.dutta67@gmail.com>
2023-05-15 13:13:22 +0530

    computation of P_1 and apk are separated

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,19 @@ fn main() {
     println!("message = {}", multisig.message);
     println!("multisig = {}", multisig.signature);
     println!("aggregated public key = {}", multisig.apk);
-    println!("p_1 = {}", multisig.p_1);
     BlsMultisig::verify(&multisig);
 }
 
 struct BlsMultisig {
     apk: G2Affine,
-    p_1: G1Affine,
+    pubkeys: Vec<G1Affine>,
     message: String,
     signature: G1Affine,
 }
 
 impl BlsMultisig {
     // compute multisig
+    // Todo: To added proof of possession test in the submitter to avoid Rogue key attack
     fn sign(message: &str) -> BlsMultisig {
         println!("Computing the multisig...");
         let g1_generator = G1Affine::generator();
@@ -38,6 +38,7 @@ impl BlsMultisig {
         let h_m: G1Affine = (g1_generator * message_scalar).into();
 
         // Compute the multisignature
+        // Todo: this will change when the submitter is refactored out from the sign code
         let mut aggr_sk: Scalar = alice_sk;
         aggr_sk += bob_sk;
         aggr_sk += charlie_sk;
@@ -51,25 +52,19 @@ impl BlsMultisig {
         let charlie_pk1: G1Affine = (g1_generator * charlie_sk).into();
         let charlie_pk2: G2Affine = (g2_generator * charlie_sk).into();
         let mut apk: G2Projective = alice_pk2.into();
-        let bob_pk2_projective: G2Projective = bob_pk2.into();
-        apk += bob_pk2_projective;
-        let charlie_pk2_projective: G2Projective = charlie_pk2.into();
-        apk += charlie_pk2_projective;
-        let apk_affine: G2Affine = apk.into();
-        let mut p_1: G1Projective = alice_pk1.into();
-        let bob_pk1_projective: G1Projective = bob_pk1.into();
-        p_1 += bob_pk1_projective;
-        let charlie_pk1_projective: G1Projective = charlie_pk1.into();
-        p_1 += charlie_pk1_projective;
-        let p_1_affine: G1Affine = p_1.into();
+        let bob_pk2: G2Projective = bob_pk2.into();
+        apk += bob_pk2;
+        let charlie_pk2: G2Projective = charlie_pk2.into();
+        apk += charlie_pk2;
+        let apk: G2Affine = apk.into();
         BlsMultisig {
-            apk: apk_affine,
-            p_1: p_1_affine,
+            apk: apk,
+            pubkeys: [alice_pk1, bob_pk1, charlie_pk1].to_vec(),
             message: message.to_string(),
             signature: sigma,
         }
     }
-
+    // Method to convert a string to a field scalar
     fn hash_to_scalar(data: &str) -> Scalar {
         let mut hasher = Sha256::new();
         hasher.update(data.as_bytes());
@@ -85,8 +80,10 @@ impl BlsMultisig {
         let g2_generator = G2Affine::generator();
         let message = multisig.message.clone();
         let apk = multisig.apk.clone();
-        let p_1 = multisig.p_1.clone();
         let sigma = multisig.signature.clone();
+        let alice_pk1 = multisig.pubkeys[0].clone();
+        let bob_pk1 = multisig.pubkeys[1].clone();
+        let charlie_pk1 = multisig.pubkeys[2].clone();
 
         // Compute alpha
         let alpha_seed = "Alpha";
@@ -95,6 +92,14 @@ impl BlsMultisig {
         // Compute the hash to curve of the message
         let message_scalar = Self::hash_to_scalar(&message);
         let h_m: G1Affine = (g1_generator * message_scalar).into();
+
+        // Compute P_1
+        let mut p_1: G1Projective = alice_pk1.into();
+        let bob_pk1_projective: G1Projective = bob_pk1.into();
+        p_1 += bob_pk1_projective;
+        let charlie_pk1_projective: G1Projective = charlie_pk1.into();
+        p_1 += charlie_pk1_projective;
+        let p_1: G1Affine = p_1.into();
 
         // compute pairing_1 (negative of the original)
         let mut pairing_1_l: G1Projective = sigma.into();
@@ -120,4 +125,3 @@ impl BlsMultisig {
         }
     }
 }
-
